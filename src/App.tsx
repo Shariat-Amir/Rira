45d50020e67d8aad0fa7bf62a17399834f7d3db8
Shariat-Amir <shariat.amir.1997@gmail.com>
2024-08-21 01:47:06 -0700

    project completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,62 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [input, setInput] = useState("");
+ 
+  const [inputCurrency,setInputCurrency]=useState(0)
   const [output, setOutput] = useState("");
+  const [isDollar , setIsDollar]=useState(true)
+  const inputRef=useRef(null)
+
+  
+const currencyVlaueHandler=(e:React.ChangeEvent<HTMLInputElement>)=>{
+const currencyValue= e.target.value
+console.log(currencyValue);
+setInputCurrency(Number(currencyValue))
+
+
+}
   
 
-  const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    console.log(input);
-    setInput(value);
-    if (value === "") {
-      setOutput("");
-    }
-  };
-
   const DolarToRial = () => {
-    if (input) {
-      const inputNumber = Number(input);
-      const outputNumber = inputNumber * 420925;
+  
+     
+      const value=Number(inputCurrency)
+      const outputNumber = 1 * value;
       setOutput(outputNumber.toString());
-    } else {
-      setOutput("لطفا عدد مورد نظر را وارد نمایید");
-    }
+      setIsDollar(true)
+   
   };
 
   const rialToDolar = () => {
-    if (input) {
-      const inputNumber = Number(input);
-      const outputNumber = inputNumber / 420925;
+    
+      
+      const value=Number(inputCurrency)
+      const outputNumber = 1 / value;
       setOutput(outputNumber.toString());
-    } else {
-      setOutput("لطفا عدد مورد نظر را وارد نمایید");
-    }
+      setIsDollar(false)
+    
+       
+    
   };
+
+  
   return (
     <main className="bg-cyan-700 w-full h-screen flex justify-center items-center font-lalezar text-2xl ">
-      <div className="bg-gray-300 w-96 h-96 flex flex-col gap-10 px-10 justify-center items-center rounded-lg shadow-lg">
-        <input
-          type="number"
-          className="rounded-lg shadow-lg text-center"
-          onChange={inputHandler}
-        />
-        <p className=" text-center">{output}</p>
+      <div className="bg-gray-300 w-96 h-96 flex flex-col gap-6 px-10 justify-center items-center rounded-lg shadow-lg">
+        
+        <input type="text"  ref={inputRef}className="rounded-lg shadow-lg text-center mt-5" placeholder="لطفا مقدار واحد دلار را وارد نمایید" onChange={currencyVlaueHandler} />
+{isDollar && output && (
+          <p className="text-center">
+            {`هریک دلار برابر با ${output} ریال میباشد`}
+          </p>
+        )}
+        {!isDollar && output && (
+          <p className="text-center">
+            {`هریک ریال برابر با ${output} دلار میباشد`}
+          </p>
+        )}       
+        
         <button
           onClick={DolarToRial}
           className="bg-blue-400 hover:bg-blue-500 p-4 rounded-lg shadow-lg text-white transition-all duration-200"
